ce4c0ccc083bf804f7c9ff8da0de8cadf363729d
Viktoriia Gorianska <viktoriia.gorianska@gmail.com>
2021-12-06 09:06:35 +0100

    small changes

--- js/journal.js
+++ js/journal.js
@@ -1,45 +1,47 @@
+console.clear()
+
 fetch('https://muc-student-companion-api.vercel.app/api/journals')
   .then((response) => response.json())
   .then((data) => {
 
     console.log(data)
 
-    data.forEach((team, index) => {
 
+
+    data.forEach((log, index) => {
       const main = document.querySelector('main')
       const section = document.createElement('section')
       section.classList.add('main__journal')
-
-      const divButton = document.createElement('div')
-      divButton.classList.add('btn__center')
-      const rateButton = document.createElement('button')
-      rateButton.classList.add('btn__rate-today body__large')
-      rateButton.innerText(`Rate Today <i class="fa-regular fa-star"></i>`)
-
-      
-
-
-
-
-
-
-      const ul = document.createElement('ul')
-      ul.classList.add('teams-list__one')
-
-
-
-      team.forEach((item, index) => {
-
-        const li = document.createElement('li')
-        li.classList.add('list-item')
-        li.innerText = item
-        ul.appendChild(li)
-      })
-
       main.appendChild(section)
-      section.appendChild(h2)
-      section.appendChild(ul)
+      section.innerHTML = `<h2 class="headline">Yesterday</h2>
+      <span class="label">Rating:</span>
+      <div class="stars">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div class="inactive"></div>
+        <div class="inactive"></div>
+      </div>
 
+      <span class="label">Comprehension:</span>
+      <div class="square">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div class="inactive"></div>
+        <div class="inactive"></div>
+      </div>
+    
+      <span class="label">Motto:</span>
+      <p class="body__large">"${log.motto}"</p>
+    
+      <span class="label">Notes:</span>
+      <p class="body__small">${log.notes}</p>`
     });
   })
 
